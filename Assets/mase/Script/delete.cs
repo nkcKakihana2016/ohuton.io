c7d3cd013ff9eede3c5c3fb450d731c87841da69
mase <nkc.yuya.mase.2016@gmail.com>
2018-09-20 13:47:10 +0900

    プロト（仮）

---
+++ Assets/mase/Script/delete.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class delete : MonoBehaviour
+{
+    public float Big;//大きさ
+    public Text count;//テキスト
+    public int countup = 0;//カウント
+    public static bool speedup = false;//スピードを上げるフラグ
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //カウントの判定
+        if (countup >= 5)
+        {
+            speedup = true;//5以上になったらtrue
+            Debug.Log("5以上になったんご");
+        }
+		
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        //当たり判定
+        if (other.gameObject.tag == "point")
+        {
+            countup += 1;//とったら1ずつ上がっていく
+            SetCount();
+            Destroy(other.gameObject);
+            //サイズ変更
+            this.transform.localScale += new Vector3(Big, Big, Big);
+        }
+    }
+
+    public void SetCount()
+    {
+        count.text = string.Format("count:{0}", countup);//反映させる
+    }
+}
