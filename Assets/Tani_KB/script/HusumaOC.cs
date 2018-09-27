40319e3f406c85d52395dab292032cd9107a33a1
DESKTOP-28F38VP\user <nkc.shota.tanikawa@gmail.com>
2018-09-27 14:58:55 +0900

    テキスト表示、メインからリザルトのシーン遷移の下書きアニメーション

---
+++ Assets/Tani_KB/script/HusumaOC.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HusumaOC : MonoBehaviour
+{
+    public Animator Husuma;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        Husuma.SetBool("open", false);
+        Husuma.SetBool("close", false);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            Husuma.SetBool("open", true);
+            Husuma.SetBool("close", false);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Husuma.SetBool("open", false);
+            Husuma.SetBool("close", true);        }
+    }
+}
--- Assets/Tani_KB/script/TextSave.cs
+++ Assets/Tani_KB/script/TextSave.cs
@@ -15,6 +15,4 @@ public class TextSave : MonoBehaviour
         text.text = str;
         inputField.text = "";
     }
-
-	
 }
