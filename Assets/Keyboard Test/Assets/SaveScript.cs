9204a731be46231e68e8c097a5c1cfea9f7d4a30
DESKTOP-28F38VP\user <nkc.shota.tanikawa@gmail.com>
2018-09-27 10:10:53 +0900

    表示だけ

---
+++ Assets/Keyboard Test/Assets/SaveScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveScript : MonoBehaviour
+{
+    string str;
+    public InputField inputField;
+    public Text text;
+
+    public void SaveText()
+    {
+        str = inputField.text;
+        text.text = str;
+        inputField.text = "";
+    }
+}
