cbb12cef205df291499a90f31c5c23986033bc00
DESKTOP-28F38VP\user <nkc.shota.tanikawa@gmail.com>
2018-10-17 15:10:34 +0900

    モバイルキーボード中断、ジャイロ操作の作成に切り替える

--- Assets/Tani_KB/script/ballRun.cs
+++ Assets/Tani_KB/script/ballRun.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ballRun : MonoBehaviour
+public class BallRun : MonoBehaviour
 {
     private float speed = 10.0f;
 
     private Vector3 dir;
 
+   
 	// Use this for initialization
 	void Start ()
     {
@@ -18,16 +19,18 @@ public class ballRun : MonoBehaviour
 	void Update ()
     {
         dir = Vector3.zero;
+       
 
         dir.x = Input.acceleration.x;
         dir.z = Input.acceleration.y;
 
-        if(dir.sqrMagnitude>1)
+       
+        if (dir.sqrMagnitude > 1)
             dir.Normalize();
 
+
         dir *= Time.deltaTime;
-
+        
         transform.Translate(dir * speed);
-
     }
 }
