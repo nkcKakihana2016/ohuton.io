f9d9158c9e5364487c78469ab30fd785fbfcdf6c
daimurata <nkc.dai.murata@gmail.com>
2018-10-22 14:31:44 +0900

    仮キャラクター追加

---
+++ Assets/Murata/scripts/TEST.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TEST : MonoBehaviour {
+
+    public int[] count_Character=new int[6];
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+	}
+}
