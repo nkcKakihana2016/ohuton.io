6a282a07bdb7d15c6685c547ae1795b784292eae
DESKTOP-28F38VP\user <nkc.shota.tanikawa@gmail.com>
2018-10-23 15:05:41 +0900

    ジャイロによる方向転換（バグあり）完成

---
+++ Assets/Tani_KB/script/InputData.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputData : MonoBehaviour
+{
+    private Quaternion gyro;
+    private GUIStyle labelStyle;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //フォント生成
+        this.labelStyle = new GUIStyle();
+        this.labelStyle.fontSize = Screen.height / 22;
+        this.labelStyle.normal.textColor = Color.white;
+
+        Input.gyro.enabled = true;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        this.gyro = Input.gyro.attitude;
+    }
+    void OnGUI()
+    {
+            float x = Screen.width / 10;
+            float y = 0;
+            float w = Screen.width * 8 / 10;
+            float h = Screen.height / 20;
+
+            for (int i = 0; i < 12; i++)
+            {
+                y = Screen.height / 10 + h * i;
+                string text = string.Empty;
+
+                switch (i)
+                {
+                    case 0://Y
+                        text = string.Format("gyro-x:{0}", this.gyro.x);
+                        break;
+                    case 1://Y
+                        text = string.Format("gyro-y:{0}", this.gyro.y);
+                        break;
+                    case 2://Y
+                        text = string.Format("gyro-z:{0}", this.gyro.z);
+                        break;
+                    case 3://Y
+                        text = string.Format("gyro-w:{0}", this.gyro.w);
+                        break;
+                    default:
+                        throw new System.InvalidOperationException();
+                }
+
+                GUI.Label(new Rect(x, y, w, h), text, this.labelStyle);
+            }
+        }
+    }
+
+
--- Assets/Tani_KB/script/Jyroball.cs
+++ Assets/Tani_KB/script/Jyroball.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class Jyroball : MonoBehaviour
 {
-    //private float rotSpeed = 10.0f;
-
-    //private Vector3 dir;
-
     float inputHorizontal = 2.0f;
     float inputVertical;
+
+    private Quaternion gyro;
 
 
     public float moveSpeed = 3f;//プレイヤーのスピード
@@ -22,18 +20,21 @@ public class Jyroball : MonoBehaviour
         child = GameObject.Find("human");
         ballRun = child.GetComponent<BallRun>();
 
+        Input.gyro.enabled = true;
     }
 
     void Update()
     {
+        this.gyro = Input.gyro.attitude;
+
         if(ballRun.DamageFlg==false)
         {
             //PlayerObjのZ軸方向に向かって進む
             transform.Translate(new Vector3(0, 0, moveSpeed) * Time.deltaTime);
 
             //左右キーで進行角度を変化させる
-            inputHorizontal = Input.GetAxisRaw("Horizontal");
-            inputVertical = Input.GetAxisRaw("Vertical");
+            //inputHorizontal = Input.GetAxisRaw("Horizontal");
+            //inputVertical = Input.GetAxisRaw("Vertical");
 
             //ジャイロ操作のメソッド呼び出し
             JyroMove();
@@ -50,21 +51,11 @@ public class Jyroball : MonoBehaviour
     //ジャイロ操作統括
     public void JyroMove()
     {
-        //    dir = Vector3.zero;
+        transform.Rotate(0, -gyro.y*10, 0);
 
-        //    dir.y = Input.acceleration.z;
-
-        //    if (dir.sqrMagnitude > 1)
-        //        dir.Normalize();
-
-        //    dir *= Time.deltaTime;
-
-        //    transform.Rotate(dir * rotSpeed);
-        transform.Rotate(0, inputHorizontal*2, 0);
-
-        if(inputVertical>0.5)
+        if (gyro.x > 0.35 ||gyro.x < -0.35)
         {
-            transform.Rotate(0, inputHorizontal * 4, 0);
+            transform.Rotate(0, gyro.y * 15, 0);
         }
     }
 }
