8475129d111df38e6109d1bd14886b411e95a2a3
daimurata <nkc.dai.murata@gmail.com>
2018-10-23 18:41:43 +0900

    配列できた。バグだらけ

---
+++ Assets/Murata/scripts/Fton_Create.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fton_Create : MonoBehaviour {
+
+    public GameObject FtonPrefab;
+	void Start ()
+    {
+		for(int X = 0; X <= 17; X++)
+        {
+            for(int Z = 0; Z <= 23; Z++)
+            {
+                Instantiate(FtonPrefab,new Vector3(-11.5f+X, 0,-17.2f+Z),transform.rotation);
+            }
+        }
+
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
