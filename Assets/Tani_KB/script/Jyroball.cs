7dc8da1f09b65d0092d27edac5fbe140c28090da
DESKTOP-28F38VP\user <nkc.shota.tanikawa@gmail.com>
2018-10-24 15:01:55 +0900

    ジャイロ難航中

--- Assets/Tani_KB/script/Jyroball.cs
+++ Assets/Tani_KB/script/Jyroball.cs
@@ -11,6 +11,7 @@ public class Jyroball : MonoBehaviour
 
 
     public float moveSpeed = 3f;//プレイヤーのスピード
+    public float inputRot;
 
     //float inputGyro;
 
@@ -23,6 +24,8 @@ public class Jyroball : MonoBehaviour
         ballRun = child.GetComponent<BallRun>();
 
         Input.gyro.enabled = true;
+
+        inputRot = 0.0f;
     }
 
     void Update()
@@ -39,33 +42,36 @@ public class Jyroball : MonoBehaviour
             //inputVertical = Input.GetAxisRaw("Vertical");
 
             //ジャイロ操作のメソッド呼び出し
-            JyroMove();
+            GyroMove();
         }
         if(ballRun.DamageFlg==true)
         {
             //PlayerObjのZ軸方向に向かって進む
             transform.Translate(new Vector3(0, 0, 0));
         }
-
     }
 
 
     //ジャイロ操作統括
-    public void JyroMove()
+    public void GyroMove()
     {
-        if(gyro.y > 0.02|| gyro.y < -0.05)
-        {
-            transform.Rotate(0, -gyro.y * 15, 0);
-        }
-
-        //if (gyro.x < -0.5)
+        //if(gyro.x<0.3||gyro.x>-0.3)
         //{
-        //    transform.Rotate(0, -gyro.y * 15, 0);
-        //}
-        //else if (gyro.x < -0.5 || gyro.y < 0.1)
-        //{
-        //    transform.Rotate(0, gyro.y * 15, 0);
-        //}
+        //    if(gyro.y<-0.09||gyro.y>-0.17)
+        //    {
+        //        transform.Rotate(0, -gyro.y * 0, 0);
+        //    }
 
+            if (gyro.y < -0.1)
+            {
+                transform.Rotate(0, -gyro.y * 15, 0);
+            }
+
+            if (gyro.y > 0.1)
+            {
+                transform.Rotate(0, -gyro.y * 15, 0);
+            }
+        //}
+       
     }
 }
