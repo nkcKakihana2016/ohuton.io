2587bf00000cc8b31d64d21d54df8c7a9a1868d5
DESKTOP-28F38VP\user <nkc.shota.tanikawa@gmail.com>
2018-10-29 15:17:27 +0900

    Timer完成、あとは素材（画像）を差し替えるだけ

--- Assets/Tani_KB/script/Timer.cs
+++ Assets/Tani_KB/script/Timer.cs
@@ -5,47 +5,50 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    public Image roadImg;
-    public Animation teacherImg;
+    public Image roadImg;　　　　　　　　　　 　//タイマー（廊下）をアタッチ
+    public Animator teacherImg;　　　　　　　　 //タイマー（先生）をアタッチ
 
-    public int cntTime = 0;
+    public float cntTime;　　　　　　　　　　　 //実際の時間制限
+    public int checkTime;　　　　　　　　　　　 //時間制限（float）をswitch文で使えるようにする変数
 
     // Use this for initialization
     void Start ()
     {
-        
-	}
+        cntTime = 0.0f;　　　　　　　　　　　　 //時間に関する数値の変数を初期化
+        checkTime = 0;
+        teacherImg.SetBool("TimerStart", false);//タイマー（先生）を起動
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (cntTime < 120)
+        //タイマー発動
+        if (cntTime<=120.0f)
         {
             TimeCounter();
         }
     }
 
+    //タイマー管理
     public void TimeCounter()
     {
-        if (Time.frameCount % 60 == 0)
-        {
-            cntTime++;
-        }
+        teacherImg.SetBool("TimerStart", true); //タイマー（先生）を起動
+        cntTime += Time.deltaTime;
+        checkTime = Mathf.CeilToInt(cntTime);   //cntTimeをint型に変更したものを保存
 
-        Debug.Log(cntTime);
-
-        switch (cntTime)
+        //指定された時間ごとの処理（cntTimeよりcheckTimeのほうが1秒早い）
+        switch (checkTime)
         {
-            case 60://制限時間が60秒になった時
+            case 61://制限時間が60秒になった時
                 Debug.Log("60秒経過");
                 break;
-            case 90://制限時間が90秒になった時
+            case 91://制限時間が90秒になった時
                 Debug.Log("90秒経過");
                 break;
-            case 105://制限時間が105秒になった時
+            case 106://制限時間が105秒になった時
                 Debug.Log("105秒経過");
                 break;
-            case 120://制限時間が120秒（ゲーム終了）になった時
+            case 121://制限時間が120秒（ゲーム終了）になった時
                 Debug.Log("ゲーム終了");
                 break;
         }
