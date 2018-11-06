0750a61a5a356a5e039d896239922b350afb8503
daimurata <nkc.dai.murata@gmail.com>
2018-11-06 13:33:55 +0900

    布団の生成ランダム化に移行中

--- Assets/Murata/scripts/Fton_Create.cs
+++ Assets/Murata/scripts/Fton_Create.cs
@@ -4,27 +4,48 @@ using UnityEngine;
 
 //布団生成スクリプト
 public class Fton_Create : MonoBehaviour {
+    List<GameObject> list = new List<GameObject>();
     //布団生成プレハブ
     public GameObject FtonPrefab;
-    public float X_count = 0f;
-    public float Z_count = 0f;
+    //初期座標を指定
+    [SerializeField]
+    Vector3 pos;
 	void Start ()
     {
         //X方向に
-		for(int X = 0; X <= 17; X++)
+		for(int X = 0; X <= 10; X++)
         {
             //Z方向に
             for(int Z = 0; Z <= 23; Z++)
             {
-                
+                var number = Random.Range(1, 101);
+                //間隔布団と布団
+                var newPos = pos + new Vector3(2 * X, 0.0f, 1.5f * Z);
                 //生成
-                Instantiate(FtonPrefab,new Vector3(-10f+X+X_count, 0.0f,-17.4f+Z+Z_count),transform.rotation);
-                X_count = 1.5f;
-                Z_count = 2f;
+                var obj = Instantiate(FtonPrefab, newPos, transform.rotation) as GameObject;
+                list.Add(obj);
+                if (number <= 30)
+                {
+                    obj.SetActive(false);
+                }
             }
         }
 
 	}
+
+    public void a()
+    {
+        foreach(GameObject obj in list)
+        {
+            var number = Random.Range(1, 101);
+            if (number <= 30)
+            {
+                obj.SetActive(true);
+            }
+            else
+                obj.SetActive(false);
+        }
+    }
 	
 	void Update ()
     {
