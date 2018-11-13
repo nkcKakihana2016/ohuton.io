b7dab2985010ddd889d93139acadfef122e594fa
daimurata <nkc.dai.murata@gmail.com>
2018-11-13 14:33:46 +0900

    ゲージのカラーパネル追加

--- Assets/Murata/scripts/Gage_Slider_Color.cs
+++ Assets/Murata/scripts/Gage_Slider_Color.cs
@@ -19,8 +19,15 @@ public class Gage_Slider_Color : MonoBehaviour {
     //距離の制限5段階
     public float One, Two, Three, Fore, Five, Six;
     public Image Gage_Slider;
+    public Image Gage_bottom;
     public float Red, Blue, Gree;
-    
+    public Color colorOne = Color.white;
+    public Color colorTwo = Color.white;
+    public Color colorThree = Color.white;
+    public Color colorFore = Color.white;
+    public Color colorFive = Color.white;
+    public Color colorSix = Color.white;
+
     void Start()
     {
         //Gg_sliderを取得
@@ -68,27 +75,45 @@ public class Gage_Slider_Color : MonoBehaviour {
         {
             case 0:
                 Camera.transform.position = new Vector3(0f, One, 0f);
-                Gage_Slider.color = new Color(Red,Gree,Blue);
+               //Gage_Slider.color = new Color(Red,Gree,Blue);
+                //Gage_bottom.color = new Color(Red,Gree,Blue);
+                Gage_bottom.color = colorOne;
+                Gage_Slider.color = colorOne;
                 break;
             case 1:
                 Camera.transform.position = new Vector3(0f, Two, 0f);
-                Gage_Slider.color = new Color(Red, Gree, Blue);
+                // Gage_Slider.color = new Color(Red, Gree, Blue);
+                //Gage_bottom.color = new Color(Red, Gree, Blue);
+                Gage_bottom.color = colorTwo;
+                Gage_Slider.color = colorTwo;
                 break;
             case 2:
                 Camera.transform.position = new Vector3(0f, Three, 0f);
-                Gage_Slider.color = new Color(Red, Gree, Blue);
+                //Gage_Slider.color = new Color(Red, Gree, Blue);
+                //Gage_bottom.color = new Color(Red, Gree, Blue);
+                Gage_bottom.color = colorThree;
+                Gage_Slider.color = colorThree;
                 break;
             case 3:
                 Camera.transform.position = new Vector3(0f, Fore, 0f);
-                Gage_Slider.color = new Color(Red, Gree, Blue);
+                //Gage_Slider.color = new Color(Red, Gree, Blue);
+                //Gage_bottom.color = new Color(Red, Gree, Blue);
+                Gage_bottom.color = colorFore;
+                Gage_Slider.color = colorFore;
                 break;
             case 4:
                 Camera.transform.position = new Vector3(0f, Five, 0f);
-                Gage_Slider.color = new Color(Red, Gree, Blue);
+                //Gage_Slider.color = new Color(Red, Gree, Blue);
+                //Gage_bottom.color = new Color(Red, Gree, Blue);
+                Gage_bottom.color = colorFive;
+                Gage_Slider.color = colorFive;
                 break;
             case 5:
                 Camera.transform.position = new Vector3(0f, Six, 0f);
-                Gage_Slider.color = new Color(Red, Gree, Blue);
+                //Gage_Slider.color = new Color(Red, Gree, Blue);
+                //Gage_bottom.color = new Color(Red, Gree, Blue);
+                Gage_bottom.color = colorSix;
+                Gage_Slider.color = colorSix;
                 break;
         }
         //sliderのvalueをゲージと同じにする
