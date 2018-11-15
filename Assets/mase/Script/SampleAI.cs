84e99391501982357471ca259bea6f71ac932e77
mase <nkc.yuya.mase.2016@gmail.com>
2018-11-15 14:52:24 +0900

    複数のターゲットを追っていくがどうやって追っていくか不明

--- Assets/mase/Script/SampleAI.cs
+++ Assets/mase/Script/SampleAI.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent( typeof( NavMeshAgent ) )]
 public class SampleAI : MonoBehaviour
 {
-    public List<GameObject> obj = new List<GameObject>(); 
-    NavMeshAgent agent;
+    //public List<GameObject> obj = new List<GameObject>(); 
+    //NavMeshAgent agent;
+    [SerializeField]
+    private Transform[] m_targets = null;
+    [SerializeField]
+    private float m_destinationThreshold = 0.0f;
 
+    private NavMeshAgent m_navAgent = null;
+
+    private int m_targetIndex = 0;
+
+    private Vector3 CurretTargetPosition
+    {
+        get
+        {
+            if (m_targets == null || m_targets.Length <= m_targetIndex)
+            {
+                return Vector3.zero;
+            }
+
+            return m_targets[m_targetIndex].position;
+        }
+    }
     // Use this for initialization
     void Start ()
     {
-        agent = GetComponent<NavMeshAgent>();
+        //agent = GetComponent<NavMeshAgent>();
+        m_navAgent = GetComponent<NavMeshAgent>();
+        m_navAgent.destination = CurretTargetPosition;
     }
 	
 	// Update is called once per frame
@@ -19,6 +42,12 @@ public class SampleAI : MonoBehaviour
     {
         //agent.destination = targetObj.transform.position;
         //agent.destination = obj.
+        if (m_navAgent.remainingDistance <= m_destinationThreshold)
+        {
+            m_targetIndex = (m_targetIndex + 1) % m_targets.Length;
+
+            m_navAgent.destination = CurretTargetPosition;
+        }
     }
 
     //public void OnTriggerEnter(Collider other)
