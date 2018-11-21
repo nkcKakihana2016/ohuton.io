ae64de00df6720d59a22f06260df648cf4721bc7
mase <nkc.yuya.mase.2016@gmail.com>
2018-11-21 14:08:17 +0900

    AI布団を追うようになったがまだPlayerを追わないので改良します

--- Assets/mase/Script/SampleAI.cs
+++ Assets/mase/Script/SampleAI.cs
@@ -3,58 +3,82 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-[RequireComponent( typeof( NavMeshAgent ) )]
+//[RequireComponent( typeof( NavMeshAgent ) )]
 public class SampleAI : MonoBehaviour
 {
-    //public List<GameObject> obj = new List<GameObject>(); 
-    //NavMeshAgent agent;
-    [SerializeField]
-    private Transform[] m_targets = null;
-    [SerializeField]
-    private float m_destinationThreshold = 0.0f;
+    private GameObject nearObj;         //最も近いオブジェクト
+    private float searchTime = 0;    //経過時間
 
-    private NavMeshAgent m_navAgent = null;
-
-    private int m_targetIndex = 0;
-
-    private Vector3 CurretTargetPosition
-    {
-        get
-        {
-            if (m_targets == null || m_targets.Length <= m_targetIndex)
-            {
-                return Vector3.zero;
-            }
-
-            return m_targets[m_targetIndex].position;
-        }
-    }
     // Use this for initialization
     void Start ()
     {
-        //agent = GetComponent<NavMeshAgent>();
-        m_navAgent = GetComponent<NavMeshAgent>();
-        m_navAgent.destination = CurretTargetPosition;
+
+        //最も近かったオブジェクトを取得
+        nearObj = serchTag(gameObject, "point");
+
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        //agent.destination = targetObj.transform.position;
-        //agent.destination = obj.
-        if (m_navAgent.remainingDistance <= m_destinationThreshold)
-        {
-            m_targetIndex = (m_targetIndex + 1) % m_targets.Length;
 
-            m_navAgent.destination = CurretTargetPosition;
+        //経過時間を取得
+        searchTime += Time.deltaTime;
+
+        if (searchTime >= 1.0f)
+        {
+            //最も近かったオブジェクトを取得
+            nearObj = serchTag(gameObject, "point");
+
+            //経過時間を初期化
+            searchTime = 0;
         }
+
+        //対象の位置の方向を向く
+        transform.LookAt(nearObj.transform);
+
+        //自分自身の位置から相対的に移動する
+        transform.Translate(Vector3.forward * 0.1f);
+
     }
 
-    //public void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.gameObject.tag == "HitBox")
-    //    {
-    //        Debug.Log("当たったんご");
-    //    }
-    //}
+    //指定されたタグの中で最も近いものを取得
+    GameObject serchTag(GameObject nowObj, string tagName)
+    {
+        float tmpDis = 0;           //距離用一時変数
+        float nearDis = 0;          //最も近いオブジェクトの距離
+        //string nearObjName = "";    //オブジェクト名称
+        GameObject targetObj = null; //オブジェクト
+
+        //タグ指定されたオブジェクトを配列で取得する
+        foreach (GameObject obs in GameObject.FindGameObjectsWithTag(tagName))
+        {
+            //自身と取得したオブジェクトの距離を取得
+            tmpDis = Vector3.Distance(obs.transform.position, nowObj.transform.position);
+
+            //オブジェクトの距離が近いか、距離0であればオブジェクト名を取得
+            //一時変数に距離を格納
+            if (nearDis == 0 || nearDis > tmpDis)
+            {
+                nearDis = tmpDis;
+                //nearObjName = obs.name;
+                targetObj = obs;
+            }
+
+        }
+        //最も近かったオブジェクトを返す
+        //return GameObject.Find(nearObjName);
+        return targetObj;
+
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "point")
+        {
+            Destroy(other.gameObject);
+            Debug.Log("お前消すんご");
+        }
+    }
 }
